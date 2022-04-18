66ec174cd067d21ba9d0c488ca4d3546a4d57717
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-18 14:12:24 +0300

    script linked

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+//this code expands and collapses the boxes
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var coll2 = document.getElementsByClassName("collapsible2");
+var i;
+
+for (i = 0; i < coll2.length; i++) {
+  coll2[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
