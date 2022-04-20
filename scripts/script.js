f6731dddf80debfc2cb983ec49d2ebdb21f6e421
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-20 17:51:22 +0300

    boxes responsiveness and smaller view port work

--- scripts/script.js
+++ scripts/script.js
@@ -14,33 +14,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
-var coll2 = document.getElementsByClassName("collapsible2");
-var i;
-
-for (i = 0; i < coll2.length; i++) {
-  coll2[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
-var coll3 = document.getElementsByClassName("collapsibleflip");
-var i;
-
-for (i = 0; i < coll3.length; i++) {
-  coll3[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
