dc9f8c628a5dd31ce5319cef84caa43942c4a3df
giulia-lir <giulia.libe3@gmail.com>
2022-04-23 21:04:20 +0300

    corrected hamburger menu, dropdown on mobile WIP

--- scripts/script.js
+++ scripts/script.js
@@ -14,14 +14,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
-// Hambuger menu onclick function
-
-function showHideMenu() {
-  var x = document.getElementById("mobileMenu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
